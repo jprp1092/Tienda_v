8fe3491c102eef0fc47fd25fbc86f5e0e9c447b4
jprp1092 <jretana80675@ufide.ac.cr>
2022-06-17 22:30:52 -0600

    Semana 5

--- src/main/java/com/tienda/controller/PersonaController.java
+++ src/main/java/com/tienda/controller/PersonaController.java
@@ -4,13 +4,18 @@
  */
 package com.tienda.controller;
 
+import com.tienda.entity.Pais;
 import com.tienda.entity.Persona;
+import com.tienda.service.IPaisService;
 import com.tienda.service.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -18,17 +23,56 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller//definimos que es tipo controller
 public class PersonaController {
+
     @Autowired
     private IPersonaService personaService;
-    
-    @GetMapping("/persona")//cuando se llame a /persona va a realizar todo lo de abajo
-    public String index (Model model){//model permite pasar informacion al html de personas
+
+    @Autowired
+    private IPaisService paisService;
+
+    //Metodo que arrastra los datos de base de datos al htnml
+    @GetMapping("/persona")//cuando se usa ese url pasa lo de abajo
+    public String index(Model model) {//model permite pasar informacion a un html
         List<Persona> listaPersona = personaService.getAllPersona();//pasamos toda la lista personas al html
-        model.addAttribute("titulo","Tabla Personas");/*donde en el html encuentre "titulo" lo va a sustituir por 
+        model.addAttribute("titulo", "Tabla Personas");/*donde en el html encuentre "titulo" lo va a sustituir por 
         "Tabla Persona" */
-        model.addAttribute("personas",listaPersona);/*donde en el html encuentre "personas" lo va a sustituir por 
+        model.addAttribute("personas", listaPersona);/*donde en el html encuentre "personas" lo va a sustituir por 
         listaPersona (line 26) */
-        return "persona";//devolvera un objeto de tipo string personas
+        return "persona";//devolvera html persona
     }
-    
+
+    //Metodo que crear una nueva persona
+    @GetMapping("/personaN")//cuando se usa ese url pasa lo de abajo
+    public String crearPersona(Model model) {//model permite pasar informacion a un html
+        List<Pais> listaPaises = paisService.listCountry();//pasamos toda la lista pais al html
+        model.addAttribute("persona", new Persona());//creamos un objeto nuevo tipo persona para poder crear la fila
+        model.addAttribute("paises", listaPaises);/*donde en el html encuentre "paises" lo va a sustituir por 
+        listaPaises (line 41) esto para poder hacer el dropdowm*/
+        return "crear";//devolvera html crear
+    }
+
+    //Metodo que guardar la nueva persona y su info
+    @PostMapping("/save")//cuando se usa ese url pasa lo de abajo
+    public String guardarPersona(@ModelAttribute Persona persona) {//enviar informacion a mi metodo 
+        personaService.savePersona(persona);//asi lo guardo en la base de datos
+        return "redirect:/persona";//redirige a persona
+    }
+
+    //Metodo que edita
+    @GetMapping("/editPersona/{id}")//cuando se usa ese url pasa lo de abajo y le estoy especificando que quiero edutar por el id
+    public String editarPersona(@PathVariable("id") Long idPersona, Model model) {//enviar informacion a mi metodo 
+        Persona persona = personaService.getPersonaById(idPersona);
+        List<Pais> listaPaises = paisService.listCountry();
+        model.addAttribute("persona", persona);//ya no creamos el objeto por que el objeto lo creamos en la linea 64
+        model.addAttribute("paises", listaPaises);
+        return "crear";//redirige a persona
+    }
+
+    //Metodo que elimina
+    @GetMapping("/delete/{id}")//cuando se usa ese url pasa lo de abajo y le estoy especificando que quiero edutar por el id
+    public String eliminarPersona(@PathVariable("id") Long idPersona) {//enviar informacion a mi metodo 
+        personaService.delete(idPersona);
+        return "redirect:/persona";//redirige a persona
+    }
+
 }
